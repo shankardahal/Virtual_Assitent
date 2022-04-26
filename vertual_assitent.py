988e3d8c7c7efaaaf9c53b8c833f1abb1363372b
shankar dahal <86343933+shankardahal@users.noreply.github.com>
2022-04-26 21:03:12 +0545

    Update vertual_assitent.py

--- vertual_assitent.py
+++ vertual_assitent.py
@@ -50,8 +50,10 @@ def run_alexa():
         talk('sorry, I have a headache')
     elif 'are you single' in command:
         talk('I am in a relationship with wifi')
+        
     elif 'joke' in command:
         talk(pyjokes.get_joke())
+        
     else:
         talk('Please say the command again.')
 
